a9959ea782b2c34e573f58deaab12984241bb078
Marco Laptop [WSL] <marco.diaz.garcia@mni.thm.de>
2022-05-04 10:28:58 +0200

    Finished with WumpusWorld Implementation

--- WumpusWorld.py
+++ WumpusWorld.py
@@ -1,9 +1,11 @@
 import random
 import os
+import bcolors
 
+#clears the Console
 def clearConsole():
     command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+    if os.name in ('nt', 'dos'):
         command = 'cls'
     os.system(command)
 
@@ -11,14 +13,65 @@ def clearConsole():
 class Agent:
     x = 0
     y = 0
+    points = 0
     bumped = False
+    hearedDeathScream = False
     arrows = 1
 
-    def getCoordinates(self):
-        return (self.x, self.y)
+    #Agent moves up, if he bumpes into a wall he saves that in "bumped"
+    def moveUp(self):
+        self.points -= 1
+        if self.y < 3:
+            self.y += 1
+            self.bumped = False
+        else:
+            self.bumped = True
 
-    def hasArrow(self):
+    #Agent moves down, if he bumpes into a wall he saves that in "bumped"
+    def moveDown(self):
+        self.points -= 1
+        if self.y > 0:
+            self.y -= 1
+            self.bumped = False
+        else:
+            self.bumped = True
+
+    #Agent moves right, if he bumpes into a wall he saves that in "bumped"
+    def moveRight(self):
+        self.points -= 1
+        if self.x < 3:
+            self.x += 1
+            self.bumped = False
+        else:
+            self.bumped = True
+
+    #Agent moves left, if he bumpes into a wall he saves that in "bumped"
+    def moveLeft(self):
+        self.points -= 1
+        if self.x > 0:
+            self.x -= 1
+            self.bumped = False
+        else:
+            self.bumped = True
+
+    #returns wether the agent has arrows left or not
+    def hasArrows(self):
         return self.arrows > 0
+
+    #If the agent has arrows left he consumes(shoots) one arrow
+    #Returning wether he was able to shoot or not
+    def shoot(self):
+        self.points -= 10
+        if self.hasArrows():
+            self.arrows -= 1
+            return True
+        else:
+            return False
+
+    #Set the "hearedDeathScream" variable
+    def hearsDeathScream(self):
+        self.hearedDeathScream = True
+
 
 #One Cell in the 4x4 gamefield
 class WumpusWorldCell:
@@ -28,24 +81,31 @@ class WumpusWorldCell:
     stench = False
     gold = False
 
-    def display(self):
+    #Returns a string representation of the Cell --> it is always 3 letters long
+    def toString(self):
         displayString = ""
+        numberAttributes = 0
         if self.pit:
-            displayString += "P"
+            displayString += bcolors.CBLUEB + "P" + bcolors.CDEFAULT
+            numberAttributes += 1
         if self.breeze:
-            displayString += "B"
+            displayString += bcolors.CBLUE + "B" + bcolors.CDEFAULT
+            numberAttributes += 1
         if self.wumpus:
-            displayString += "W"
+            displayString += bcolors.CREDB + "W" + bcolors.CDEFAULT
+            numberAttributes += 1
         if self.stench:
-            displayString += "S"
+            displayString += bcolors.CRED + "S" + bcolors.CDEFAULT
+            numberAttributes += 1
         if self.gold:
-            displayString += "G"
+            displayString += bcolors.CDARKYELLOWB + "G" + bcolors.CDEFAULT
+            numberAttributes += 1
 
-        if len(displayString) == 0:
+        if numberAttributes == 0:
             displayString = "   "
-        elif len(displayString) == 1:
+        elif numberAttributes == 1:
             displayString = " " + displayString + " "
-        elif len(displayString) == 2:
+        elif numberAttributes == 2:
             displayString += " "
 
         return displayString
@@ -54,6 +114,8 @@ class WumpusWorldCell:
 class WumpusWorld:
     agent = Agent()
 
+    #Initiating the WumpusWorldField with the 3 pits and corresponding breezes
+    #the wumpus and its stenches as well as the gold
     def __init__(self):
         self.world = []
 
@@ -110,97 +172,129 @@ class WumpusWorld:
 
                 i += 1
 
-
+    #Returns wether the given cell contains a pit or not
     def cellIsPit(self, x, y):
         return self.world[y][x].pit
 
+    #Returns wether the given cell contains a breeze or not
     def cellIsBreeze(self, x, y):
         return self.world[y][x].breeze
 
+    #Returns wether the given cell contains the wumpus or not
     def cellIsWumpus(self, x, y):
         return self.world[y][x].wumpus
 
+    #Returns wether the given cell contains a stench or not
     def cellIsStench(self, x, y):
         return self.world[y][x].stench
 
+    #Returns wether the given cell contains the gold or not
     def cellIsGold(self, x, y):
         return self.world[y][x].gold
 
+    #Returns wether the given cell contains the agent or not
     def cellIsAgent(self, x, y):
         return x == self.agent.x and y == self.agent.y
 
-    def moveAgentUp(self):
-        if self.agent.y < 3:
-            self.agent.y += 1
-            self.agent.bumped = False
-        else:
-            self.agent.bumped = True
+    #Agent shoots an arrow up
+    #If the Wumpus is being hit, the agent hears its death scream
+    def shootUp(self):
+        self.agent.shoot()
+        x = self.agent.x
+        for y in range(self.agent.y, 4):
+            if self.cellIsWumpus(x, y):
+                self.agent.hearsDeathScream()
 
-    def moveAgentDown(self):
-        if self.agent.y > 0:
-            self.agent.y -= 1
-            self.agent.bumped = False
-        else:
-            self.agent.bumped = True
+    #Agent shoots an arrow down
+    #If the Wumpus is being hit, the agent hears its death scream
+    def shootDown(self):
+        self.agent.shoot()
+        x = self.agent.x
+        for y in range(self.agent.y, -1, -1):
+            if self.cellIsWumpus(x, y):
+                self.agent.hearsDeathScream()
 
-    def moveAgentRight(self):
-        if self.agent.x < 3:
-            self.agent.x += 1
-            self.agent.bumped = False
-        else:
-            self.agent.bumped = True
+    #Agent shoots an arrow right
+    #If the Wumpus is being hit, the agent hears its death scream
+    def shootRight(self):
+        self.agent.shoot()
+        y = self.agent.y
+        for x in range(self.agent.x, 4):
+            if self.cellIsWumpus(x, y):
+                self.agent.hearsDeathScream()
 
-    def moveAgentLeft(self):
-        if self.agent.x > 0:
-            self.agent.x -= 1
-            self.agent.bumped = False
-        else:
-            self.agent.bumped = True
+    #Agent shoots an arrow left
+    #If the Wumpus is being hit, the agent hears its death scream
+    def shootLeft(self):
+        self.agent.shoot()
+        y = self.agent.y
+        for x in range(self.agent.x, -1, -1):
+            if self.cellIsWumpus(x, y):
+                self.agent.hearsDeathScream()
 
+    #Returns wether the game is lost or not
     def gameOver(self):
-        return self.cellIsPit(self.agent.x, self.agent.y) or self.cellIsWumpus(self.agent.x, self.agent.y)
+        return self.cellIsPit(self.agent.x, self.agent.y) or (self.cellIsWumpus(self.agent.x, self.agent.y) and not self.agent.hearedDeathScream)
 
+    #Return wether the game is won or not
     def gameWon(self):
         return self.cellIsGold(self.agent.x, self.agent.y)
 
+    #starts the game and waits for the users actions
     def play(self):
         while not (self.gameOver() or self.gameWon()):
             clearConsole()
-            self.display();
-            action = input("Action? [move/shoot]: ")
-            direction = input("Direction? [up/down/left/right]: ")
+            self.displayHidden();
 
-            if action == "move":
-                if direction == "up":
-                    self.moveAgentUp()
-                elif direction == "down":
-                    self.moveAgentDown()
-                elif direction == "right":
-                    self.moveAgentRight()
-                elif direction == "left":
-                    self.moveAgentLeft()
-            elif action == "shoot":
-                if direction == "up":
+            correctInput = False
+            while not correctInput:
+                action = input("Action? [move/shoot]: ")
+                correctInput = action in ("move", "m", "shoot", "s")
+
+            correctInput = False
+            while not correctInput:
+                direction = input("Direction? [up/down/left/right]: ")
+                correctInput = direction in ("up", "u", "down", "d", "left", "l", "right", "r")
+
+            if action in ("move", "m"):
+                if direction in ("up", "u"):
+                    self.agent.moveUp()
+                elif direction in ("down", "d"):
+                    self.agent.moveDown()
+                elif direction in ("right", "r"):
+                    self.agent.moveRight()
+                elif direction in ("left", "l"):
+                    self.agent.moveLeft()
+            elif action in ("shoot", "s"):
+                if direction in ("up", "u"):
                     self.shootUp()
-                elif direction == "down":
+                elif direction in ("down", "d"):
                     self.shootDown()
-                elif direction == "right":
+                elif direction in ("right", "r"):
                     self.shootRight()
-                elif direction == "left":
+                elif direction in ("left", "l"):
                     self.shootLeft()
 
         clearConsole()
+        if self.gameOver():
+            self.agent.points -= 1000
+            print(bcolors.CRED + bcolors.CBOLD + "GAME OVER" + bcolors.CDEFAULT)
+        elif self.gameWon():
+            self.agent.points += 1000
+            print(bcolors.CGREEN + bcolors.CBOLD + "CONGRATULATIONS" + bcolors.CDEFAULT)
+
+        print("You finished with ", self.agent.points, " points.\n")
         self.display();
 
-
+    #Displays the whole WumpusWorldField in the Console
     def display(self):
         displayString = "+-------+-------+-------+-------+\n"
         for y in range(3,-1,-1):
             displayString += "|"
             for x in range(4):
-                displayString += " " + self.world[y][x].display()
-                if x == self.agent.x and y == self.agent.y:
-                    displayString += " A |"
+                displayString += " " + self.world[y][x].toString()
+                if self.cellIsAgent(x, y):
+                    displayString += " " + bcolors.CGREEN + bcolors.CBLINK + "A" + bcolors.CDEFAULT + " |"
                 else:
                     displayString += "   |"
 
@@ -208,9 +302,31 @@ class WumpusWorld:
 
         print(displayString)
 
+        if self.agent.bumped:
+            print("Agent bumped into a wall!\n")
+        if self.agent.hearedDeathScream:
+            print("Agent heard wumpus death scream!\n")
 
-agent = Agent()
+    #Displays the WumpusWorldField but only the Cell containing the Agent is visible
+    def displayHidden(self):
+        displayString = "+-------+-------+-------+-------+\n"
+        for y in range(3,-1,-1):
+            displayString += "|"
+            for x in range(4):
+                if self.cellIsAgent(x, y):
+                    displayString += " " + self.world[y][x].toString() + " " + bcolors.CGREEN + bcolors.CBLINK + "A" + bcolors.CDEFAULT + " |"
+                else:
+                    displayString += "       |"
+
+            displayString += "\n+-------+-------+-------+-------+\n"
+
+        print(displayString)
+
+        if self.agent.bumped:
+            print("Agent bumped into a wall!\n")
+        if self.agent.hearedDeathScream:
+            print("Agent heard wumpus death scream!\n")
+
 
 wumpusWorld = WumpusWorld()
-
 wumpusWorld.play()
